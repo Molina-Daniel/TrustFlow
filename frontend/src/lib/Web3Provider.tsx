e873ca05f9b7d8bca4118d8c0ff0fade90f93dd4
Daniel Molina <danielmolina.dev@gmail.com>
2025-05-10 19:09:54 +0100

    feat: add polkadot westend asset hub network on provider

--- frontend/src/lib/Web3Provider.tsx
+++ frontend/src/lib/Web3Provider.tsx
@@ -2,11 +2,39 @@ import React from "react";
 import { WagmiProvider, createConfig } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+import { http, type Chain } from "viem";
+
+export const westendAssetHub = {
+  id: 420420421,
+  name: "Westend Asset Hub",
+  nativeCurrency: {
+    name: "Westend",
+    symbol: "WND",
+    decimals: 12,
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://westend-asset-hub-eth-rpc.polkadot.io"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Subscan",
+      url: "https://westend-asset-hub-eth-explorer.parity.io",
+    },
+  },
+  testnet: true,
+} as const satisfies Chain;
 
 const config = createConfig(
   getDefaultConfig({
-    appName: "",
+    chains: [westendAssetHub],
+    transports: {
+      [westendAssetHub.id]: http(),
+    },
+    appName: "TrustFlow",
     walletConnectProjectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID!,
+    enableFamily: false,
   })
 );
 
